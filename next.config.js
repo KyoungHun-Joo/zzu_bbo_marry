0fc457738a49cf895895c97057dcd7511b5382c1
KyoungHun Joo <wnrudgns73@naver.com>
2024-06-06 00:56:29 +0900

    test3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 
+  bundleAnalyzerConfig: {
+    server: {
+      analyzerMode: "static",
+      reportFilename: "./server.html",
+    },
+    browser: {
+      analyzerMode: "static",
+      reportFilename: "./client.html",
+    },
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
@@ -12,4 +27,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([[withBundleAnalyzer]], nextConfig);
