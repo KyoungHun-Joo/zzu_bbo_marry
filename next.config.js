4112d179408622650b65c1d577aa797f6b64ef14
KyoungHun Joo <wnrudgns73@naver.com>
2024-06-06 00:38:00 +0900

    test

--- next.config.js
+++ next.config.js
@@ -6,18 +6,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
-  analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-  analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-  bundleAnalyzerConfig: {
-    server: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/server.html",
-    },
-    browser: {
-      analyzerMode: "static",
-      reportFilename: "../bundles/client.html",
-    },
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
