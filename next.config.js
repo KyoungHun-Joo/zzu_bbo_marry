8a54d707c0b1b3cfc14f3b206f398491c6293687
KyoungHun Joo <wnrudgns73@naver.com>
2024-06-23 18:47:05 +0900

    cors atest

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://zzubbomarry.s3.ap-northeast-2.amazonaws.com" }, // replace this your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
   typescript: {
