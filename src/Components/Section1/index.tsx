f6200a19e0dad818c65ac8d91a2b5668d9d5fcfe
KyoungHun Joo <wnrudgns73@naver.com>
2024-10-17 21:57:17 +0900

    style update

--- src/Components/Section1/index.tsx
+++ src/Components/Section1/index.tsx
@@ -18,10 +18,10 @@ export const Section1: React.FunctionComponent<any> = ({ mode }) => {
     <Container
       id="hero"
       className={`${
-        mode === "party" ? "party" : mode == "2" ? "normal" : ""
+        mode === "party" ? "party" : mode == "2" ? "normal" : "heroMain"
       } hero w-100 h-100 p-3 mx-auto text-center d-flex justify-content-center align-items-center text-white overlay-content`}
     >
-      <main id="heroMain">
+      <main id="heroMain" style={{ marginBottom: "15%" }}>
         <h4 style={{ color: "#d1d1d1" }}>우 리 결 혼 합 니 다</h4>
         <h1 style={{ color: "#d1d1d1" }}>주경훈 & 한보라</h1>
         <p style={{ color: textColor }}>2024. 12. 7. (토) 오후 3:30</p>
